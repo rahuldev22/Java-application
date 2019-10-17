fd5b4d7e86bd987ebf8436cbbb509c87ba72bf80
rahuldev22 <56692435+rahuldev22@users.noreply.github.com>
2019-10-17 22:10:46 +0530

    Java Program to check Even or Odd

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
 
-package javaapplication1;
-
-interface xyz
+class CheckEvenOdd
 {
-    void show();
-}
+  public static void main(String args[])
+  {
+    int num;
+    System.out.println("Enter an Integer number:");
 
-public class JavaApplication1 
-{
-    public static void main(String[] args) 
-    {
-        xyz obj = () ->System.out.println("keep Learning");  
-        obj.show();
-    }
-    
+    //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
 }
